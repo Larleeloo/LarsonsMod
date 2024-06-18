0318354d6c052db1a0a1ce3234078d5895df4f71
Larleeloo <140470964+Larleeloo@users.noreply.github.com>
2024-06-18 13:17:46 -0700

    Commit 1

---
+++ src/main/java/net/larson/larsonsmod/LarsonsModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.larson.larsonsmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class LarsonsModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
